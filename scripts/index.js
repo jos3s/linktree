384682e7a06ba8bfe22749938779625463a03c8a
Jos3s <karatedkgt17@gmail.com>
2021-06-30 16:39:23 -0300

    Tema escuro e separação de arquivos

--- scripts/index.js
+++ scripts/index.js
@@ -13,3 +13,11 @@ discord.addEventListener("click", () => {
     })
     .catch((e) => alert(e.message));
 });
+
+const theme = document.getElementById("switch");
+const html = document.documentElement;
+console.log(html);
+
+theme.addEventListener("change", ({ target: { checked } }) => {
+  checked ? html.classList.add("dark") : html.classList.remove("dark");
+});
