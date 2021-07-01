94a2826dcad2cb85e2202d571c455e03e4022f4b
Jos3s <karatedkgt17@gmail.com>
2021-07-01 19:50:42 -0300

    Adicionando tema persistente e atualizando cores dos links sociais

--- scripts/index.js
+++ scripts/index.js
@@ -14,10 +14,31 @@ discord.addEventListener("click", () => {
     .catch((e) => alert(e.message));
 });
 
-const theme = document.getElementById("switch");
+const themeSwitch = document.getElementById("switch");
 const html = document.documentElement;
 console.log(html);
 
-theme.addEventListener("change", ({ target: { checked } }) => {
-  checked ? html.classList.add("dark") : html.classList.remove("dark");
+const localTheme = localStorage.getItem("@jos3sLinktree/Theme");
+console.log(localTheme);
+if (localTheme) {
+  if (localTheme === "dark") {
+    html.classList.add("dark");
+    themeSwitch.checked = true;
+  } else {
+    html.classList.remove("dark");
+    themeSwitch.checked = false;
+  }
+} else {
+  localStorage.setItem("@jos3sLinktree/Theme", "light");
+}
+
+themeSwitch.addEventListener("change", ({ target: { checked } }) => {
+  if (checked) {
+    html.classList.add("dark");
+    localStorage.setItem("@jos3sLinktree/Theme", "dark");
+  } else {
+    html.classList.remove("dark");
+    localStorage.setItem("@jos3sLinktree/Theme", "light");
+  }
 });
+
